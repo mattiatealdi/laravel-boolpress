e7616e7095c57ec2936c41d8323d5faf5f8c3eee
mattiatealdi <mattiatealdi@gmail.com>
2021-07-01 19:01:34 +0200

    commit

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -15,8 +16,8 @@ class PostController extends Controller
     public function index()
     {
         //
-
-        return view('admin.posts.index');
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -49,6 +50,7 @@ class PostController extends Controller
     public function show($id)
     {
         //
+
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,6 +3,26 @@
 @section('content')
 <div class="container">
     <h1>I miei Posts</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th colspan="3">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
+                    <td>{{ $post->id }}</td>
+                    <td>{{ $post->title }}</td>
+                    <td><a class="btn btn-primary" href="{{ route('admin.posts.show', $post) }}">SHOW</a></td>
+                    <td><a class="btn btn-secondary" href="{{ route('admin.posts.edit', $post) }}">EDIT</a></td>
+                    <td><a class="btn btn-danger" href="{{ route('admin.posts.destroy', $post) }}">DELETE</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
 
